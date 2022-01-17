9001e66095c9bac79a8f2b80e955227b32655533
Thiago Lima <thiagofslima@live.com>
2022-01-17 18:09:29 -0300

    Adicionar arquivos de projeto.

---
+++ FrmConsultarCEP.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ConsultaCEP
+{
+    partial class FrmConsultarCEP
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mtxCEP = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLogradouro = new System.Windows.Forms.TextBox();
+            this.btnConsultarCEP = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // mtxCEP
+            // 
+            this.mtxCEP.BeepOnError = true;
+            this.mtxCEP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtxCEP.Location = new System.Drawing.Point(12, 28);
+            this.mtxCEP.Mask = "00000-000";
+            this.mtxCEP.Name = "mtxCEP";
+            this.mtxCEP.ResetOnPrompt = false;
+            this.mtxCEP.ResetOnSpace = false;
+            this.mtxCEP.Size = new System.Drawing.Size(94, 23);
+            this.mtxCEP.TabIndex = 0;
+            this.mtxCEP.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CEP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Logradouro:";
+            // 
+            // txtLogradouro
+            // 
+            this.txtLogradouro.Location = new System.Drawing.Point(12, 94);
+            this.txtLogradouro.Name = "txtLogradouro";
+            this.txtLogradouro.ReadOnly = true;
+            this.txtLogradouro.Size = new System.Drawing.Size(261, 20);
+            this.txtLogradouro.TabIndex = 3;
+            // 
+            // btnConsultarCEP
+            // 
+            this.btnConsultarCEP.Location = new System.Drawing.Point(172, 21);
+            this.btnConsultarCEP.Name = "btnConsultarCEP";
+            this.btnConsultarCEP.Size = new System.Drawing.Size(104, 37);
+            this.btnConsultarCEP.TabIndex = 4;
+            this.btnConsultarCEP.Text = "Consultar";
+            this.btnConsultarCEP.UseVisualStyleBackColor = true;
+            this.btnConsultarCEP.Click += new System.EventHandler(this.btnConsultarCEP_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Bairro:";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(12, 134);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.ReadOnly = true;
+            this.txtBairro.Size = new System.Drawing.Size(261, 20);
+            this.txtBairro.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Cidade:";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(12, 174);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.ReadOnly = true;
+            this.txtCidade.Size = new System.Drawing.Size(203, 20);
+            this.txtCidade.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(218, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Estado:";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(221, 174);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.ReadOnly = true;
+            this.txtEstado.Size = new System.Drawing.Size(52, 20);
+            this.txtEstado.TabIndex = 3;
+            // 
+            // FrmConsultarCEP
+            // 
+            this.AcceptButton = this.btnConsultarCEP;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 211);
+            this.Controls.Add(this.btnConsultarCEP);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtLogradouro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mtxCEP);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmConsultarCEP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consultar CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox mtxCEP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLogradouro;
+        private System.Windows.Forms.Button btnConsultarCEP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEstado;
+    }
+}
+
---
+++ FrmConsultarCEP.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsultaCEP
+{
+    public partial class FrmConsultarCEP : Form
+    {
+        public FrmConsultarCEP()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultarCEP_Click(object sender, EventArgs e)
+        {
+            if(mtxCEP.MaskCompleted)
+            {
+                using (var ws = new WSCorreios.AtendeClienteClient())
+                {
+                    try
+                    {
+                        var resultado = ws.consultaCEP(mtxCEP.Text);
+                        txtLogradouro.Text = resultado.end;
+                        txtBairro.Text = resultado.bairro;
+                        txtCidade.Text = resultado.cidade;
+                        txtEstado.Text = resultado.uf;
+
+                    }
+                    catch (System.ServiceModel.FaultException)
+                    {
+
+                        MessageBox.Show("CEP não encontrado!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+                MessageBox.Show("CEP inválido!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
